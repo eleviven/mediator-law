253fe3fa7749c0a6cddebf21e394328c30dd1ca6
eleviven <firuz.haciyeff@gmail.com>
2021-08-06 16:18:12 +0400

    Add contact title statically

--- src/components/breadcrumb/breadcrumb.js
+++ src/components/breadcrumb/breadcrumb.js
@@ -14,6 +14,7 @@ const convertBreadcrumb = (string) => {
       .replace("home", "Ana Səhifə")
       .replace("services", "Xidmətlər")
       .replace("articles", "Məqalələr")
+      .replace("contact", "Əlaqə")
   );
 };
 
